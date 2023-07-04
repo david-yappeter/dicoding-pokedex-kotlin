39dbe35133e35c18e48e85c40eccb2c2fac29b05
David Yappeter <davidyap11les@gmail.com>
2023-07-04 15:10:05 +0700

    feat: add testing fetch image and about page

--- app/src/main/java/playground/example/dicoding_pokedex/AboutActivity.kt
+++ app/src/main/java/playground/example/dicoding_pokedex/AboutActivity.kt
@@ -2,10 +2,19 @@ package playground.example.dicoding_pokedex
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class AboutActivity : ComponentActivity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val actionbar = supportActionBar
+        actionbar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
--- app/src/main/java/playground/example/dicoding_pokedex/MainActivity.kt
+++ app/src/main/java/playground/example/dicoding_pokedex/MainActivity.kt
@@ -1,11 +1,18 @@
 package playground.example.dicoding_pokedex
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import playground.example.dicoding_pokedex.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +24,32 @@ class MainActivity : ComponentActivity() {
         binding.btnIntent.setOnClickListener {
             val intent = Intent(this, AboutActivity::class.java)
             startActivity(intent)
+        }
+
+        DownloadImageFromInternet(binding.testImage).execute("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
+    }
+
+    private inner class DownloadImageFromInternet(var imageView: ImageView): AsyncTask<String, Void, Bitmap?>() {
+        init {
+            Toast.makeText(applicationContext, "Please wait, it may take a few seconds...", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun doInBackground(vararg urls: String?): Bitmap? {
+            val imageURL = urls[0]
+            var image: Bitmap? = null
+
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+            } catch (e: Exception) {
+                Log.e("Error Message", e.message.toString())
+                e.printStackTrace()
+            }
+            return image
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            imageView.setImageBitmap(result)
         }
     }
 }
